312804a909803fdacc4f6dc6654d4a9e71754473
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-03-02 15:25:21 +0530

    login added

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -62,6 +62,41 @@ export const signup = async (req: Request, res: Response) => {
   }
 };
 
-export const login = async (req: Request, res: Response) => {};
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: 'All fields are required',
+      });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid email or password',
+      });
+    }
+    const token = signToken(user._id.toString());
+
+    res.cookie('jwt', token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      httpOnly: true, // prevents XSS attacks
+      sameSite: 'strict', // prevents CSRF attacks
+      secure: process.env.NODE_ENV === 'production',
+    });
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log('Error in login controller:', error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
 
 export const logout = async (req: Request, res: Response) => {};
