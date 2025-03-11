7771e96bd55d968293f4cefb01bfcfc89708dc00
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-03-11 10:53:13 +0530

    user routes done

--- be/routes/userRoutes.js
+++ be/routes/userRoutes.js
@@ -1,5 +1,8 @@
 import Router from 'express';
+import { protectRoute } from '../middleware/auth';
 
 const userRouter = Router();
+
+userRouter.put('/update', protectRoute, updateProfile);
 
 export default userRouter;
