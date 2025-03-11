706ae32f1109e48d0178ff7b0648e2a14687c9bf
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-03-11 13:18:04 +0530

    match route

--- be/routes/matchRoutes.js
+++ be/routes/matchRoutes.js
@@ -1,5 +1,18 @@
-import Router from 'express';
+import express from 'express';
+import { protectRoute } from '../middleware/auth.js';
+import {
+  getMatches,
+  getUserProfiles,
+  swipeLeft,
+  swipeRight,
+} from '../controllers/matchController.js';
 
-const matchRouter = Router();
+const matchRouter = express.Router();
+
+matchRouter.post('/swipe-right/:likedUserId', protectRoute, swipeRight);
+matchRouter.post('/swipe-left/:dislikedUserId', protectRoute, swipeLeft);
+
+matchRouter.get('/', protectRoute, getMatches);
+matchRouter.get('/user-profiles', protectRoute, getUserProfiles);
 
 export default matchRouter;
