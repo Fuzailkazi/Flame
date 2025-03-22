7f8af7c2b3447dd4f41b24f95c0a401c73104570
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-03-22 07:03:39 +0530

    message routes added

--- be/routes/messageRoutes.js
+++ be/routes/messageRoutes.js
@@ -1,5 +1,15 @@
 import Router from 'express';
+import { protectRoute } from '../middleware/auth.js';
+import {
+  sendMessage,
+  getConversation,
+} from '../controllers/messageController.js';
 
 const messagesRouter = Router();
+
+messagesRouter.use(protectRoute);
+
+messagesRouter.post('/send', sendMessage);
+messagesRouter.get('/conversation/:userId', getConversation);
 
 export default messagesRouter;
--- be/routes/userRoutes.js
+++ be/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import Router from 'express';
-import { protectRoute } from '../middleware/auth';
-
+import { protectRoute } from '../middleware/auth.js';
+import { updateProfile } from '../controllers/userController.js';
 const userRouter = Router();
 
 userRouter.put('/update', protectRoute, updateProfile);
