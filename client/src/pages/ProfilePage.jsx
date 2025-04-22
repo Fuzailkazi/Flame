6627ca7f1f9a5e5714e99ce2973b44580443500f
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-04-22 20:18:08 +0530

    Header / nav done

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
-
+import { Header } from '../components/Header';
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className='min-h-screen bg-gray-50 flex flex-col'>
+      <Header />
+    </div>
+  );
 };
 
 export default ProfilePage;
