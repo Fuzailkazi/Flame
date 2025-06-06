3fa49f20cc192e3b1958a57e94709a1e246a16c1
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2025-06-06 16:45:27 +0530

    seed users generation

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAuthStore } from '../hooks/useAuthHook';
 import Sidebar from '../components/Sidebar';
 import { Header } from '../components/Header';
+import { useMatchHook } from '../hooks/useMatchHook';
 const HomePage = () => {
+  const { isLoadingUserProfiles, getUserProfiles, userProfiles } =
+    useMatchHook();
+
+  useEffect(() => {
+    getUserProfiles();
+  }, [getUserProfiles]);
+
+  console.log(userProfiles);
+
   const { logout } = useAuthStore();
   return (
     <div className='flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-200 to-purple-200 overflow-hidden'>
-      <Sidebar></Sidebar>
+      <Sidebar />
     </div>
   );
 };
